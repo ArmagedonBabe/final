8701288b3e4132ed4b8f22953d65711577ad8ab5
Amir <210103034@stu.sdu.edu.kz>
2022-12-18 20:37:25 +0600

    js photo

--- subPages/about.js
+++ subPages/about.js
@@ -10,3 +10,14 @@ window.addEventListener("scroll", function(){
   }
   lastScrollTop = scrollTop;
 })
+
+document.querySelectorAll('.mainPage img').forEach(image =>{
+image.onclick = () =>{
+  document.querySelector('.popup-image').style.display = 'block';
+  document.querySelector('.popup-image img').src = image.getAttribute('src');
+}
+
+});
+document.querySelector('.popup-image span').onclick = () =>{
+  document.querySelector('.popup-image').style.display = 'none';
+}
